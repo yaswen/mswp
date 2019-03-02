9fcc0736bd5938c3f92f7ece11d5f65d1d2c51b8
yaswen <yaswen@163.com>
2019-03-02 22:20:45 +0800

    build

---
+++ build/precache-manifest.409ccdb8309a0fafbdb93f40a3a1b546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8228725c224a5135afe5",
+    "url": "/mswp/static/js/runtime~main.8228725c.js"
+  },
+  {
+    "revision": "fb221827d20eb0588d43",
+    "url": "/mswp/static/js/main.fb221827.chunk.js"
+  },
+  {
+    "revision": "7fa0dbffcdd0b78ba137",
+    "url": "/mswp/static/js/2.7fa0dbff.chunk.js"
+  },
+  {
+    "revision": "fb221827d20eb0588d43",
+    "url": "/mswp/static/css/main.735e3fa3.chunk.css"
+  },
+  {
+    "revision": "7fa0dbffcdd0b78ba137",
+    "url": "/mswp/static/css/2.dc8b88bb.chunk.css"
+  },
+  {
+    "revision": "1a191bc0dcf5df5ef7b3f7e17526a0a2",
+    "url": "/mswp/index.html"
+  }
+];
